ecbf9a5638e9a06c913723196368f252b1f46083
ombalapure7 <65531892+ombalapure7@users.noreply.github.com>
2021-11-07 13:13:07 +0530

    Add files via upload

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,14 +1,25 @@
 import { useRef } from "react";
 import { useGlobalContext } from "../context";
 import "./Jumbotron.css";
+import { AiOutlineClear } from "react-icons/ai";
+import { BsSearch } from "react-icons/bs";
+
 
 function Jumbotron() {
-  const { preventSubmit, setSearchValue } = useGlobalContext();
+  const { preventSubmit, setSearchValue, fetchUser } = useGlobalContext();
   const searchInput = useRef();
 
   function searchUser() {
     console.log("Search for this username:", searchInput.current.value);
     setSearchValue(searchInput.current.value);
+  }
+
+  function clearSearchBar() {
+    console.log("Search input cleared...");
+    if (!(searchInput.current.value === "")) {
+      console.log("Nothing to clear...");
+      fetchUser();
+    }
   }
 
   return (
@@ -22,7 +33,8 @@ function Jumbotron() {
             placeholder="Search user..."
             ref={searchInput}
           />
-          <button className="btn" onClick={searchUser}>Search</button>
+          <button className="btn" onClick={searchUser}><BsSearch/></button>
+          <button className="btn-clear" onClick={clearSearchBar}><AiOutlineClear/></button>
         </div>
       </form>
     </section>
